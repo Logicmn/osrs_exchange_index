91759c7d576b7671fab312b42dfe1c1bcd213957
Logicmn <zeke@sweetmojo.com>
2018-08-16 18:54:51 -0400

    Removed wtf.quickform due to complication with csrf token

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 from flask import Flask, render_template, flash
 from flask_bootstrap import Bootstrap
 from wtforms import StringField
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from grand_exchange.exchange import Item
 
 app = Flask(__name__)
 app.secret_key = 'supersecret'
 bootstrap = Bootstrap(app)
 
-class SubmitForm(Form):
+class SubmitForm(FlaskForm):
     input = StringField('Item search')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = SubmitForm()
+    print(form.errors)
+
     if form.validate_on_submit():
         id = form.input.data
+
         try:
             name = Item.name(id)
             price = Item.price(id)
@@ -28,11 +31,13 @@ def index():
             return render_template('index.html',form=form, name=name, price=price, members=members,
             buy_avg=buy_avg, sell_avg=sell_avg, buy_vol=buy_vol, sell_vol=sell_vol,
             total_vol=total_vol)
+
         except KeyError:
+            print("keyerror")
             flash('This item you have searched is not listed on the Grand Exchange.')
             return render_template('index.html', form=form)
-    else:
-        return render_template('index.html', form=form)
+
+    return render_template('index.html', form=form)
 
 @app.errorhandler(404)
 def page_not_found(e):
