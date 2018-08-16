1f68a4eb790b7b7aae7fb6a2abe8e6e1185ebac9
Logicmn <zeke@sweetmojo.com>
2018-08-16 02:48:45 -0400

    Added the start of a search functionality

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template, flash
 from flask_bootstrap import Bootstrap
+from wtforms import StringField
+from flask_wtf import Form
 from grand_exchange.exchange import Item
 
 app = Flask(__name__)
 app.secret_key = 'supersecret'
 bootstrap = Bootstrap(app)
 
+class SubmitForm(Form):
+    item = StringField('Item search')
+
 @app.route('/')
 def index():
     id = '28'
+    form = SubmitForm()
     try:
         name = Item.name(id)
         price = Item.price(id)
@@ -18,12 +24,12 @@ def index():
         buy_vol = Item.buy_vol(id)
         sell_vol = Item.sell_vol(id)
         total_vol = Item.total_vol(id)
-        return render_template('index.html', name=name, price=price, members=members,
+        return render_template('index.html',form=form, name=name, price=price, members=members,
         buy_avg=buy_avg, sell_avg=sell_avg, buy_vol=buy_vol, sell_vol=sell_vol,
         total_vol=total_vol)
     except KeyError:
         flash('This item you have searched is not listed on the Grand Exchange.')
-        return render_template('index.html')
+        return render_template('index.html', form=form)
 
 @app.errorhandler(404)
 def page_not_found(e):
