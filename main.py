cd37e5d25c1a151c86c4f8c53e8c243f7746e8ce
Logicmn <zeke@sweetmojo.com>
2018-08-16 18:20:28 -0400

    Item search capability added

--- main.py
+++ main.py
@@ -9,26 +9,29 @@ app.secret_key = 'supersecret'
 bootstrap = Bootstrap(app)
 
 class SubmitForm(Form):
-    item = StringField('Item search')
+    input = StringField('Item search')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    id = '28'
     form = SubmitForm()
-    try:
-        name = Item.name(id)
-        price = Item.price(id)
-        members = Item.members(id)
-        buy_avg = Item.buy_avg(id)
-        sell_avg = Item.sell_avg(id)
-        buy_vol = Item.buy_vol(id)
-        sell_vol = Item.sell_vol(id)
-        total_vol = Item.total_vol(id)
-        return render_template('index.html',form=form, name=name, price=price, members=members,
-        buy_avg=buy_avg, sell_avg=sell_avg, buy_vol=buy_vol, sell_vol=sell_vol,
-        total_vol=total_vol)
-    except KeyError:
-        flash('This item you have searched is not listed on the Grand Exchange.')
+    if form.validate_on_submit():
+        id = form.input.data
+        try:
+            name = Item.name(id)
+            price = Item.price(id)
+            members = Item.members(id)
+            buy_avg = Item.buy_avg(id)
+            sell_avg = Item.sell_avg(id)
+            buy_vol = Item.buy_vol(id)
+            sell_vol = Item.sell_vol(id)
+            total_vol = Item.total_vol(id)
+            return render_template('index.html',form=form, name=name, price=price, members=members,
+            buy_avg=buy_avg, sell_avg=sell_avg, buy_vol=buy_vol, sell_vol=sell_vol,
+            total_vol=total_vol)
+        except KeyError:
+            flash('This item you have searched is not listed on the Grand Exchange.')
+            return render_template('index.html', form=form)
+    else:
         return render_template('index.html', form=form)
 
 @app.errorhandler(404)
