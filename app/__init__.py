092d0ef73d75f2ff626297948905387ac756f121
Logicmn <zeke@sweetmojo.com>
2018-08-17 20:04:29 -0400

    Added local error logging

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import logging
+from logging.handlers import RotatingFileHandler
+import os
 
 from config import Config
 
@@ -10,5 +13,18 @@ app.config.from_object(Config)
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
 
 from app import routes, models
