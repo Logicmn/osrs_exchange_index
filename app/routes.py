8918da66a9cc4cdc62b5375ed39fddb9671011a1
Logicmn <zeke@sweetmojo.com>
2018-08-19 22:57:18 -0400

    Improved search functionality (can now search by partial strings)

--- app/routes.py
+++ app/routes.py
@@ -11,7 +11,7 @@ from app.models import Item
 @app.route('/', methods=['GET', 'POST'])
 def index():
     # Update the database
-    update()
+    #update()
 
     form = SubmitForm()
     print(form.errors)
@@ -19,7 +19,7 @@ def index():
 
     if form.validate_on_submit():
         item_name = str.lower(form.input.data)
-        item = Item.query.filter_by(name=item_name).first()
+        item = Item.query.filter(Item.name.contains(item_name)).first()
         if item != None:
             return render_template('index.html',form=form, id=item.game_id, name=item.name.capitalize(), price=item.price, members=item.members,
             buy_avg=item.buy_avg, sell_avg=item.sell_avg, buy_vol=item.buy_vol, sell_vol=item.sell_vol,
